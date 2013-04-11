b59a0a74a9f034a81426feb1066ef79b7eab5687
Ben Schwartz <ben.schwartz@bancvue.com>
2013-04-11 10:54:09 -0500

    updates

--- example/app/init.php
+++ example/app/init.php
@@ -1,13 +1,18 @@
 <?php
 namespace MyApp;
 
-use Fliglio\App\Defaults;
+use Fliglio\Flfc\Context;
+use Fliglio\Flfc\Request;
+use Fliglio\Flfc\Response;
 use Fliglio\Flfc as flfc;
+use Fliglio\Flfc\NamespaceFcChainResolver;
 use Fliglio\Flfc\DefaultFcChainResolver;
 use Fliglio\Flfc\FcChainFactory;
 use Fliglio\Flfc\FcChainRunner;
 use Fliglio\Routing as routing;
 use Fliglio\Routing\RouteMap;
+use Fliglio\Web\Uri;
+use Fliglio\Web\HttpAttributes;
 
 
 error_reporting(E_ALL | E_STRICT);
@@ -17,38 +22,35 @@ ini_set("display_errors" , 1);
 // require_once dirname(dirname(dirname(__FILE__))) . '/vendor/autoload.php';
 include 'Example/Services.php';
 
-/* set up Fliglio\Web\HttpAttributes */
-Defaults::initWebDefaults();
 
-/* Fliglio\Flfc\Context */
-$context = Defaults::getContextDefault();
+HttpAttributes::apacheDefaults();
 
 
 
 // Configure Routing
-RouteMap::setRoutes(array(
-	"api" => new routing\PatternRoute('/api/:command', array(
+$routeMap = new RouteMap();
+$routeMap
+	->connect("api", new routing\PatternRoute('/api/:command', array(
 		'ns'           => 'MyApp\Example',
 		'commandGroup' => 'Services',
-	)),
-	"rest" => new routing\PatternRoute('/api/rest/:command', array(
+	)))
+	->connect("rest", new routing\PatternRoute('/api/rest/:command', array(
 		'ns'           => 'MyApp\Example',
 		'commandGroup' => 'Services',
 		'_restful'     => true,
-	)),
-	"baz" => new routing\StaticRoute('/api/static/baz', array(
+	)))
+	->connect("baz", new routing\StaticRoute('/api/static/baz', array(
 		'cmd' => 'MyApp\Example.Services.baz',
-	)),
-	"hello" => new routing\PatternRoute('/api/hello/:target', array(
+	)))
+	->connect("hello", new routing\PatternRoute('/api/hello/:target', array(
 		'cmd' => 'MyApp\Example.Services.hello',
-	)),
-	"error" => new routing\CatchNoneRoute(array(
+	)))
+	->connect("error", new routing\CatchNoneRoute(array(
 		'cmd' => 'MyApp\Example.Services.handleError',
-	)),
-	"404" => new routing\CatchAllRoute(array(
+	)))
+	->connect("404", new routing\CatchAllRoute(array(
 		'cmd' => 'MyApp\Example.Services.pageNotFound',
-	)),
-));
+	)));
 
 
 
@@ -56,12 +58,13 @@ RouteMap::setRoutes(array(
 
 
 // Configure Front Controller Chain & Default Resolver
+$htmlChain = new flfc\HttpApp(new flfc\ServeHtmlApp(dirname(__FILE__) . '/index.html'));
+$apiChain  = new flfc\HttpApp(new routing\UriLintApp(new routing\RoutingApp(new routing\RestInvokerApp(), $routeMap)));
 
-$chain = new flfc\HttpApp(new routing\RoutingApp(new routing\RestInvokerApp()));
-$resolver = new DefaultFcChainResolver($chain);
-FcChainFactory::addResolver($resolver);
-
+FcChainFactory::addResolver(new DefaultFcChainResolver($htmlChain));
+FcChainFactory::addResolver(new NamespaceFcChainResolver($apiChain, 'api'));
 
 // Run App
+$context = new Context(Request::createDefault(), new Response());
 $chainRunner = new FcChainRunner();
-$chainRunner->dispatchRequest($context);
+$chainRunner->dispatchRequest($context, "@404", "@error");
