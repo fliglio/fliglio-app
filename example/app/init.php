cc23ea17a5a2ff5d369cde5212d552bf51565ec1
Ben Schwartz <ben.schwartz@bancvue.com>
2013-04-11 11:26:14 -0500

    updates

--- example/app/init.php
+++ example/app/init.php
@@ -19,7 +19,7 @@ error_reporting(E_ALL | E_STRICT);
 ini_set("display_errors" , 1);
 
 
-// require_once dirname(dirname(dirname(__FILE__))) . '/vendor/autoload.php';
+require_once dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))) . '/vendor/autoload.php';
 include 'Example/Services.php';
 
 
