22aa969856452e5f9f98d8a6096768a89fa4963e
Ben Schwartz <benschw@gmail.com>
2015-02-27 10:52:09 -0600

    refactor(http): adding status codes, protocol, and method constants

--- src/MyApp/HtmlExample/Errors.php
+++ src/MyApp/HtmlExample/Errors.php
@@ -3,6 +3,7 @@
 namespace MyApp\HtmlExample;
 
 use Fliglio\Http\ResponseWriter;
+use Fliglio\Http\Http;
 
 use Fliglio\Flfc\DefaultBody;
 
@@ -12,14 +13,14 @@ class Errors {
 	}
 	
 	public function handleError(ResponseWriter $resp) {
-		$resp->setStatus(500);
+		$resp->setStatus(Http::STATUS_INTERNAL_SERVER_ERROR);
 		return new DefaultBody(sprintf(
 			'<h3>Error</h3><pre>%s</pre>',
 			$req->getProp('exception')
 		));
 	}
 	public function handlePageNotFound(ResponseWriter $resp) {
-		$resp->setStatus(404);
+		$resp->setStatus(Http::STATUS_NOT_FOUND);
 		return new DefaultBody('<h3>Page Not Found</h3>');
 	}
 	
