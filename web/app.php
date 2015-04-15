138e0326e74fdc58372395a2e4baa3db976eb483
Ben Schwartz <benschw@gmail.com>
2015-04-15 14:32:01 -0500

    updates for symphony validation

--- web/app.php
+++ web/app.php
@@ -20,13 +20,17 @@ use Fliglio\Routing\Type\RouteBuilder;
 use Fliglio\Routing\DefaultInvokerApp;
 use Fliglio\Http\Http;
 
-use MyApp\RestExample\FooApi\FooApiMapper;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
 error_reporting(E_ALL | E_STRICT);
 ini_set("display_errors" , 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
+error_log(dirname(__DIR__) . "/vendor/symfony/validator");
+AnnotationRegistry::registerAutoloadNamespace(
+	'Symfony\\Component\\Validator\\Constraints\\', 
+	dirname(__DIR__) . "/vendor/symfony/validator"
+);
 
 // Configure Routing
 $restRouteMap = new RouteMap();
