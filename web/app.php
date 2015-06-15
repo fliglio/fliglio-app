bc96649a1702391a4b86cbb9d6d4f538f1d9cca3
Ben Schwartz <benschw@gmail.com>
2015-06-15 17:26:37 +0000

    whitespace cleanup

--- web/app.php
+++ web/app.php
@@ -2,20 +2,10 @@
 namespace MyApp;
 
 
-
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
 error_reporting(E_ALL | E_STRICT);
 ini_set("display_errors" , 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-error_log(dirname(__DIR__) . "/vendor/symfony/validator");
-AnnotationRegistry::registerAutoloadNamespace(
-	'Symfony\\Component\\Validator\\Constraints\\',
-	dirname(__DIR__) . "/vendor/symfony/validator"
-);
-
 
 
 $svc = new RestExampleApplication();
