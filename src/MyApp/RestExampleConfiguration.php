eae2bc80f00a22435d9c9e368855943cbf6247c9
Ben Schwartz <benschw@gmail.com>
2015-06-14 20:11:41 -0500

    configuring routes with resource inst

--- src/MyApp/RestExampleConfiguration.php
+++ src/MyApp/RestExampleConfiguration.php
@@ -5,24 +5,26 @@ namespace MyApp;
 use Fliglio\Http\Http;
 use Fliglio\Routing\Type\RouteBuilder;
 use Fliglio\Fli\DefaultConfiguration;
+use MyApp\RestExample\FooResource;
 
 class RestExampleConfiguration extends DefaultConfiguration {
 
 	public function getRoutes() {
+		$resource = new FooResource();
 		return [
 			RouteBuilder::get()
 				->uri('/api/foo/:id')
-				->command('MyApp\RestExample.FooResource.getFoo')
+				->resource($resource, 'getFoo')
 				->method(Http::METHOD_GET)
 				->build(),
 			RouteBuilder::get()
 				->uri('/api/foo')
-				->command('MyApp\RestExample.FooResource.addFoo')
+				->resource($resource, 'addFoo')
 				->method(Http::METHOD_POST)
 				->build(),
 			RouteBuilder::get()
 				->uri('/api/foo')
-				->command('MyApp\RestExample.FooResource.getAllFoos')
+				->resource($resource, 'getAllFoos')
 				->method(Http::METHOD_GET)
 				->build(),
 		];
